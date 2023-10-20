5f95bc2b1fcafc15def7c57ff2e264e823b4fdc7
Denis B <bulavatsky@list.ru>
2023-10-20 10:07:43 +0300

    AsyncForm.js ready

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -13,15 +13,23 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
-
+    if (element) {
+      this.element = element;
+    } else {
+      throw new Error('Error: element not found');
+    }
+    this.registerEvents();
   }
 
   /**
    * Необходимо запретить отправку формы и в момент отправки
-   * вызывает метод submit()
+   * вызывать метод submit()
    * */
   registerEvents() {
-
+    this.element.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.submit();
+    });
   }
 
   /**
@@ -32,11 +40,15 @@ class AsyncForm {
    * }
    * */
   getData() {
-
+    const formData = new FormData(this.element);
+    const formSend = {};
+    for (let [key, value] of formData.entries()) {
+      formSend[key] = value;
+    }
+    return formSend;
   }
 
-  onSubmit(options){
-
+  onSubmit(options) {
   }
 
   /**
@@ -44,6 +56,6 @@ class AsyncForm {
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    this.onSubmit(this.getData());
   }
 }
